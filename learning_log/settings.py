382fb737d08a1ae4fe38786f85423698ee07d9d3
xiaosun <xiaosun@pinduoduo.com>
2021-07-05 22:51:33 +0800

    modify staticfiles_dirs

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -142,6 +142,4 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static')
-    )
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
