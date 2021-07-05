b6a84cc369e44d887faa1fba939b01351b2a190b
xiaosun <xiaosun@pinduoduo.com>
2021-07-05 09:36:31 +0800

    Disabla Collectstatic

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,6 +135,7 @@ BOOTSTRAP3 = {
 
 if os.getcwd() == '/app':
     import dj_database_url
+    DISABLE_COLLECTSTATIC = 1
     DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost')
     }
